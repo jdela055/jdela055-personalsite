a5033de6bb106255b010dce8609c7f11dff2fe7c
Julian Dela Rosa-Billings <jdela055@ucr.edu>
2025-01-09 22:31:56 -0800

    content changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -21,11 +21,14 @@ export default function AboutMe() {
                 {/* <p> Professionally amatuer! 😎 </p> */}
                 <p>
                     Welcome to my humble little React Native site! 😎
-                    I hope you like the colors :) my partner helped me pick them :) and the photo of my dog :) His name is Chewie and he's really cool and I love him :) He's wearing a crown in the photo because it was his birthday :)
+                    I hope you like the colors, my partner helped me pick them :) and the photo of my dog :) His name is Chewie and he's really cool and I love him :) He's wearing a crown in the photo because it was his birthday :)
                 </p>
+                <br/>
                 <p>
-                👉👈 
-                    I'm like looking for a job or something so it sure would be nice if a super cool employer went onto this here site and decided to give me a job :)
+                    This site is a work in progress, I'll keep updating it over time.                    
+                    <br/>
+                    👉👈 
+                    I'm like looking for a job or something so it sure would be nice and cool if a super cool employer went onto this here site and decided to give me a job :)
                 </p>
 
             </CardBody>
